99edb0cba4ec03e55fd1408eb152626796aa5c17
BarbarBetsy <barbarbetsy@googlemail.com>
2018-10-25 23:01:20 +0200

    add js for hamburger menu

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+function myFunction() {
+    var menu = document.getElementById("menu");
+menu.classList.toggle("open");
+console.log("ok");
+}
+
+function myFunctionClose() {
+    var menu = document.getElementById("menu");
+menu.classList.toggle("open");
+console.log("ok");
+} 
